f5b07e814b172fd7710fa42a31a51b3c35e9063d
KUMAR KARTIKAY <kartikay.maa@gmail.com>
2025-01-22 19:21:49 +0530

    final code lab 1

--- aimllab1.java
+++ aimllab1.java
@@ -1,31 +1,31 @@
-
 import java.util.Scanner;
+import java.util.Random;
+
 class aimllab1 {
     static int movecount = 0;
-    static char currentplayer ='X';
+    static char currentplayer = 'X';
     static char[][] board = new char[3][3];
-        // aiml
+
     public static void main(String[] args) {
         initializeBoard();
         
-        while(true){
+        while (true) {
             printBoard();
-            if(gameover()){
+            if (gameover()) {
                 break;
             }
             playermove();
         }
-        
     }
-    public static void initializeBoard(){
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
+
+    public static void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 board[i][j] = ' ';
             }
         }
     }
-    
-    // Print the current state of the board
+
     public static void printBoard() {
         System.out.println("Current Board:");
         for (int i = 0; i < 3; i++) {
@@ -35,9 +35,9 @@ class aimllab1 {
             System.out.println();
         }
     }
-    
-    public static boolean gameover(){
-         if (posewin()) {
+
+    public static boolean gameover() {
+        if (posewin()) {
             printBoard();
             System.out.println("Player " + currentplayer + " wins!");
             return true;
@@ -47,12 +47,11 @@ class aimllab1 {
             return true;
         }
 
-        // Change player for the next turn
         currentplayer = (currentplayer == 'X') ? 'O' : 'X';
         return false;
     }
-    
-     public static void make2(int row, int col) {
+
+    public static void makeMove(int row, int col) {
         if (board[row][col] == ' ') {
             board[row][col] = currentplayer;
             movecount++;
@@ -60,9 +59,8 @@ class aimllab1 {
             System.out.println("Cell is already occupied, try again.");
         }
     }
-    // check if player wins
+
     public static boolean posewin() {
-        // Check rows and columns for a win
         for (int i = 0; i < 3; i++) {
             if (board[i][0] == currentplayer && board[i][1] == currentplayer && board[i][2] == currentplayer) {
                 return true;
@@ -71,36 +69,57 @@ class aimllab1 {
                 return true;
             }
         }
-        // Check diagonals for a win
+
         if (board[0][0] == currentplayer && board[1][1] == currentplayer && board[2][2] == currentplayer) {
             return true;
         }
         if (board[0][2] == currentplayer && board[1][1] == currentplayer && board[2][0] == currentplayer) {
             return true;
         }
+
         return false;
     }
-    public static void playermove(){
-        movecount++;
-        Go(movecount);
+
+    public static void playermove() {
+        if (currentplayer == 'X') {
+            playerTurn();
+        } else {
+            computerTurn();
+        }
     }
-    
-    // Function to handle a move by the current player (Go(N)) // taking input
-    public static void Go(int moveNumber) {
+
+    public static void playerTurn() {
         Scanner scanner = new Scanner(System.in);
         int row = -1;
         int col = -1;
 
         while (true) {
-            System.out.println("Player " + currentplayer + ", enter row (0-2) and column (0-2) to make move (" + moveNumber + "): ");
+            System.out.println("Player " + currentplayer + ", enter row (0-2) and column (0-2) to make move: ");
             row = scanner.nextInt();
             col = scanner.nextInt();
 
             if (row >= 0 && row <= 2 && col >= 0 && col <= 2 && board[row][col] == ' ') {
-                make2(row, col);
+                makeMove(row, col);
                 break;
             } else {
                 System.out.println("Invalid move. Try again.");
+            }
+        }
+    }
+
+    public static void computerTurn() {
+        Random random = new Random();
+        int row, col;
+
+        System.out.println("Computer is making a move...");
+
+        while (true) {
+            row = random.nextInt(3);
+            col = random.nextInt(3);
+
+            if (board[row][col] == ' ') {
+                makeMove(row, col);
+                break;
             }
         }
     }
